9a152beb9fe4ce13ea25843a55cc01954ad08544
Oleksii <oleksii.marianchenko@gmail.com>
2023-05-11 18:55:57 +0300

    created portfolio site

--- 034_quizzler/data.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-properties = {'amount': 2,
-              'type': 'boolean'}
-
-response = requests.get(url='https://opentdb.com/api.php', params=properties)
-
-question_data = response.json()['results']
